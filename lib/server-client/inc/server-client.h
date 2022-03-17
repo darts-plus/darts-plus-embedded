f0e420f9834b20e4d341dddd58a08fe0d33e1bc4
Artur Ziółkowski <aaartiii99@gmail.com>
2022-03-17 22:24:33 +0100

    Display class with menu_display and score_display

--- lib/server-client/inc/server-client.h
+++ lib/server-client/inc/server-client.h
@@ -3,8 +3,8 @@
 
 #include <Arduino.h>
 #include <ArduinoJson.h>
-#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFiMulti.h>
 #include <Hash.h>
 #include <SocketIOclient.h>
