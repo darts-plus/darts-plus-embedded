d0d95c288c5ce08c699d3d4f51edc224a3d40074
Bartosz <kwiatkowsky.kontakt@gmail.com>
2022-03-18 16:46:25 +0100

    Simple example of usage EEPROMWearLevel library

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,73 +1,24 @@
-/*
- * WebSocketClientSocketIO.ino
- *
- *  Created on: 06.06.2016
- *
- */
-
 #include <Arduino.h>
-#include <ArduinoJson.h>
-#include <ESP8266HTTPClient.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <Hash.h>
-#include <SocketIOclient.h>
-#include <WebSocketsClient.h>
-#include "game-api.h"
-#include "server-client.h"
-void error_handler(const size_t &error_nr) {
-}
-
-std::shared_ptr<ServerClient> client;
-std::shared_ptr<GameApi> game;
-
+#include <EEPROMWearLevel.h>
 
 void setup() {
-    // USE_SERIAL.begin(921600);
-    USE_SERIAL.begin(9600);
+  Serial.begin(9600);
+  EEPROMwl.begin(0, 2);
+  // write
+  EEPROMwl.update(0, 12);
+  long var2 = 33333;
+  EEPROMwl.put(1, var2);
 
-    // Serial.setDebugOutput(true);
-    USE_SERIAL.setDebugOutput(true);
+  // read
+  byte var1 = EEPROMwl.read(0);
+  Serial.print(F("var1: "));
+  Serial.println(var1);
 
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-
-    for (uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
-    }
+  long var3 = -1;
+  EEPROMwl.get(1, var3);
+  Serial.print(F("var2: "));
+  Serial.println(var3);
 }
-bool first = true;
+
 void loop() {
-    if (first) {
-        first = false;
-        client = std::make_shared<ServerClient>("multimedia_plastek", "123454321", "192.168.0.21", 8000);
-        USE_SERIAL.print("[HTTP] Requesting setting...");
-        auto raw_settings = client->RequestSettings(this_board_id);
-        while (raw_settings == String()) {
-            USE_SERIAL.print(".");
-            raw_settings = client->RequestSettings(this_board_id);
-            delay(500);
-        }
-        StaticJsonDocument<SIZE_SETTINGS_JSON> doc;
-        deserializeJson(doc, raw_settings);
-        Settings set(doc);
-
-        USE_SERIAL.println("==============================================");
-
-        game = std::make_shared<GameApi>(set);
-        client->game_id = game->id;
-        USE_SERIAL.println();
-        USE_SERIAL.println("[GAME] Game Initialized");
-    }
-    
-    while (game->status != GameStatus_Finished) {
-        if (not client->JoinedGame()) return;
-        client->loop();
-        game->loop();
-      
-    }
-    // first = true;
 }
