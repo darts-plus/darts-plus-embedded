35cafa127830519d089c0b117091618421074cc1
Bartosz <kwiatkowsky.kontakt@gmail.com>
2022-03-18 18:08:37 +0100

    Testing new library and simple example

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,24 +1,25 @@
-#include <Arduino.h>
-#include <EEPROMWearLevel.h>
+#include <EEPROMex.h>
+#include <EEPROMVar.h>
 
-void setup() {
-  Serial.begin(9600);
-  EEPROMwl.begin(0, 2);
-  // write
-  EEPROMwl.update(0, 12);
-  long var2 = 33333;
-  EEPROMwl.put(1, var2);
-
-  // read
-  byte var1 = EEPROMwl.read(0);
-  Serial.print(F("var1: "));
-  Serial.println(var1);
-
-  long var3 = -1;
-  EEPROMwl.get(1, var3);
-  Serial.print(F("var2: "));
-  Serial.println(var3);
+void setup()
+{
+  Serial.begin(9600); 
+  delay(100); 
+  EEPROMVar<float> eepromFloat(5.5);
+  EEPROMVar<double> eepromDouble(1.33356);
+  EEPROMVar<char> eepromChar('H');
+  eepromFloat.save(); 
+  eepromDouble.save();
+  eepromChar.save();
+  Serial.println(eepromFloat.getAddress());
+  Serial.println(eepromDouble.getAddress());
+  Serial.println(eepromChar.getAddress());
+  Serial.println(eepromFloat);
+  Serial.println(eepromDouble);
+  Serial.println(eepromChar);
 }
 
-void loop() {
+void loop()
+{
+
 }
